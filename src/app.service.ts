2a11464a238879093233e3bb4488ec5e1a9effc8
Abdul-Rahimm <k213951@nu.edu.pk>
2025-07-03 14:40:08 +0500

    fixed some ESLINT related errors

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,3 +6,10 @@ export class AppService {
     return 'Hello World!';
   }
 }
+
+@Injectable()
+export class GoodbyeService {
+  getGoodbye(): string {
+    return 'Goodbye World from service!';
+  }
+}
