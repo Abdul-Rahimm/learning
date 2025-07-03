72070bb4d242a861ab1844bece54d5f73fe1df49
Abdul-Rahimm <k213951@nu.edu.pk>
2025-07-03 15:55:45 +0500

    controller logic chp2 done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+  */
+
+  @Get() // GET /users or /users?role=admin
+  findAll(@Query('role') role?: 'admin' | 'user') {
+    return ['all users', role];
+  }
+
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post() // POST /users
+  create(@Body() user: object) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { ...userUpdate, id };
+  }
+
+  @Delete(':id') // GET /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
