6943362e5f01d6f7aed61e3282e79d0abf52c593
Abdul-Rahimm <k213951@nu.edu.pk>
2025-07-04 16:53:22 +0500

    services now injected in controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
@@ -18,29 +19,30 @@ export class UsersController {
     PATCH /users/:id
     DELETE /users/:id
   */
+  constructor(private readonly usersService: UsersService) {}
 
   @Get() // GET /users or /users?role=admin
   findAll(@Query('role') role?: 'admin' | 'user') {
-    return ['all users', role];
+    return this.usersService.findAll(role);
   }
 
   @Get(':id') // GET /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersService.findone(parseInt(id, 10));
   }
 
   @Post() // POST /users
-  create(@Body() user: object) {
-    return user;
+  create(@Body() user: { name: string; role: 'admin' | 'user' }) {
+    return this.usersService.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
   update(@Param('id') id: string, @Body() userUpdate: object) {
-    return { ...userUpdate, id };
+    return this.usersService.update(parseInt(id, 10), userUpdate);
   }
 
   @Delete(':id') // GET /users/:id
   delete(@Param('id') id: string) {
-    return { id };
+    return this.usersService.delete(parseInt(id, 10));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    { id: 1, name: 'John Doe', role: 'admin' },
+    { id: 2, name: 'Jane Smith', role: 'user' },
+    { id: 3, name: 'Alice Johnson', role: 'user' },
+    { id: 4, name: 'Bob Brown', role: 'admin' },
+  ];
+
+  findAll(role?: 'admin' | 'user') {
+    if (role) return this.users.filter((user) => user.role === role);
+
+    return this.users;
+  }
+
+  findone(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  create(user: { name: string; role: 'admin' | 'user' }) {
+    const newUser = { id: this.users.length + 1, ...user };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  update(id: number, userUpdate: { name?: string; role?: 'admin' | 'user' }) {
+    const user = this.findone(id);
+    if (!user) return null;
+
+    Object.assign(user, userUpdate);
+    return user;
+  }
+
+  delete(id: number) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) return null;
+
+    const deletedUser = this.users.splice(userIndex, 1);
+    return deletedUser[0];
+  }
+}
